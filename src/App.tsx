e6c880ac143b509f23de2f23a9b3fb3a9bd90f64
ali <alioliaee31@gmail.com>
2023-08-31 11:50:22 +0330

    add request

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-import './App.css'
+import { gql, useQuery } from "@apollo/client";
+import { client } from "./adaptor/client";
+import CountryList from "./components/country-list";
 
 function App() {
+  const LIST_COUNTRIES = gql`
+  {
+    countries {
+      name
+      code
+      emoji
+      native
+      currency
+      capital
+      languages {
+        code
+        name
+      }
+    }
+  }
+  `;
+
+  const {data} = useQuery(LIST_COUNTRIES, {client});
 
   return (
-    <div></div>
+   <CountryList {...data}/>
   )
 }
 
